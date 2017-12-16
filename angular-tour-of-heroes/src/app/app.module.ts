cecb963cf20f2bbd5db9bca3c8496cfe4af0c1e1
bastien.rominger.anium <bastien.rominger.anium@gmail.com>
2017-12-16 05:00:24 -0500

    add news

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { NewsComponent } from './news/news.component';
+
+import { NewsService } from './news.service';
+import { NewsDetailComponent } from './news-detail/news-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    NewsComponent,
+    NewsDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NewsService],
+  bootstrap: [NewsComponent]
 })
 export class AppModule { }
